8ba079ae12a5856fb552cc483304eba782ddda9f
Robert Hong <robert.h.hong@gmail.com>
2021-09-30 21:26:57 -0400

    added navbar and content

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,25 @@
-// scripts go here
+var tabs = document.querySelectorAll("nav a");
+var contentPara = document.querySelector('.content')
+
+contentPara.innerHTML = cuisines;
+
+// for (let i = 0; i < tabs.length; i++) {
+//     tabs[i].addEventListener("click", makeActive);
+//   }
+
+// tabs.forEach(function(tab){
+//     tab,addEventListener("click", makeActive)
+// } )
+
+tabs.forEach(
+    tab => addEventListener("click", makeActive) );
+  
+  function makeActive(event) {
+    makeInactive();
+    event.target.classList.add("active")
+    event.preventDefault(); 
+  }
+
+  function makeInactive(){
+    tabs.forEach( tab => tab.classList.remove("active"));
+  }
